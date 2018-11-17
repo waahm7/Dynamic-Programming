1719f79627545d6c68ba51fb0cfc16b2b1cbb093
waahm7 <waahm7@gmail.com>
2018-11-17 12:26:53 +0500

    9-Bug fix when last element is not in LIS

--- top-50/src/LongestIncreasingSeqeunce_9.java
+++ top-50/src/LongestIncreasingSeqeunce_9.java
@@ -7,21 +7,25 @@ public class LongestIncreasingSeqeunce_9 {
         dp[0]=0;
         if(arr[0]>=0)
             dp[1]=1;
+        int max=1;
         for (int i=1;i<dp.length;i++){
             for(int j=i-1;j>0;j--){
-                if(arr[j-1]<=arr[i-1])
+                if(arr[j-1]<=arr[i-1]){
                     dp[i]=Math.max(dp[i],dp[j]+1);
+                    if(dp[i]>max)
+                        max=dp[i];
+                }
             }
 
         }
 
         System.out.println(Arrays.toString(dp));
-        return dp[dp.length-1];
+        return max;
     }
 
 
     public static void main(String[] agrs){
-        int[] A = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
+        int[] A = { 0, 8, 4, 12, 2};
 
         System.out.println(LIS_length(A));
     }
